39c0a4d24e467ddd9d3b2fc172a3a7753a52b43a
GHOME <1120314609@qq.com>
2022-01-15 15:53:27 +0800

    'first'

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
+import path from 'path'
+dotenv.config( { path: path.join( __dirname, '..', '.env' ) } )
+
+const main = () => {
+  const app = express()
+  app.use( cors() )
+  const PORT = process.env.PORT
+  app.use( express.static( path.join( __dirname, '..', 'public' ) ) )
+  app.use( express.json() )
+  app.use( express.urlencoded( { extended: true } ) )
+
+  app.get( '/', ( req: Request, res: Response ) => {
+    res.send( 'Hello World!' )
+    console.log(req);
+    
+  } )
+
+  app.listen( PORT, () => {
+    console.log( `Server on http://localhost:${PORT}` )
+  } )
+}
+
+
+main()
